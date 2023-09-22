e0739468d5f3f22078a4bfa94df13adf3d330f7c
Rajdeep <rajdeepmukherjee2002@gmail.com>
2023-09-22 18:50:52 +0530

    added validation for the fields

--- src/main/java/com/user/api/entities/Name.java
+++ src/main/java/com/user/api/entities/Name.java
@@ -2,10 +2,14 @@ package com.user.api.entities;
 
 import jakarta.annotation.Nullable;
 import jakarta.persistence.Embeddable;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Null;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.antlr.v4.runtime.misc.NotNull;
 
 @Data
 @Embeddable
@@ -13,8 +17,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class Name {
+    @NotBlank
     private String f_name;
     @Nullable
     private String m_name;
+    @NotEmpty
     private String l_name;
 }
--- src/main/java/com/user/api/entities/User.java
+++ src/main/java/com/user/api/entities/User.java
@@ -1,6 +1,7 @@
 package com.user.api.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,9 +18,17 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int userId;
+
     @Embedded
     private Name name;
+
+    @Min(value = 18,message = "User must be at least 18+ years")
+    @Max(value = 120,message = "Are you kidding me!")
     private int age;
+
+    @NotBlank(message = "email cannot be null or empty")
+    @Email(message = "Enter a valid email")
+    @Column(unique = true)
     private String email;
 
     @OneToMany(mappedBy = "user",cascade = CascadeType.ALL,fetch = FetchType.EAGER)
